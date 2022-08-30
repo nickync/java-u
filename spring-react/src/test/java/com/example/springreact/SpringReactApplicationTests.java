f65b603a9bd8ea7857db27d028238da0829cbc53
nick <lizheng9024@gmail.com>
2022-08-29 20:59:09 -0400

    spring react

--- spring-react/src/test/java/com/example/springreact/SpringReactApplicationTests.java
+++ spring-react/src/test/java/com/example/springreact/SpringReactApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.springreact;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringReactApplicationTests {
 
 	@Test
 	void contextLoads() {
