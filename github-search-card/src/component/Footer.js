82d0752a3c188fdf13d8411eb74f1b32c0d5fc11
nickync <lizheng9024@gmail.com>
2022-10-29 21:58:22 -0400

    update footer

--- github-search-card/src/component/Footer.js
+++ github-search-card/src/component/Footer.js
@@ -4,9 +4,9 @@ export default class
  extends Component {
   render() {
     return (
-      <div className='text-end'>
-         <footer className='footer absolute-bottom me-1'>
-            <span className='text-muted'>Pluralsight project 2022 @Zen</span>
+      <div className='text-center'>
+         <footer className='footer absolute-bottom me-1 mt-2'>
+            <span className='text-muted'> All rights reserved 2022 @ Zen</span>
         </footer>
       </div>
     )
