ff99a2f26a75e7c6ba0ec55bd36cd21beb9ab14a
nickync <lizheng9024@gmail.com>
2022-12-05 21:54:19 -0500

    sp

--- Udemy-Java/lesson-1/src/main/java/com/example/lesson1/helloworld/App02HelloWorldSpring.java
+++ Udemy-Java/lesson-1/src/main/java/com/example/lesson1/helloworld/App02HelloWorldSpring.java
@@ -1,6 +1,8 @@
-package com.example.lesson1;
+package com.example.lesson1.helloworld;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
@@ -9,5 +11,10 @@ public class App02HelloWorldSpring {
 
         //2: Configure the things that we want spring to manage - @Configuration
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean("person2methodCall"));
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- Udemy-Java/lesson-1/src/main/java/com/example/lesson1/helloworld/HelloWorldConfiguration.java
+++ Udemy-Java/lesson-1/src/main/java/com/example/lesson1/helloworld/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
-package com.example.lesson1;
+package com.example.lesson1.helloworld;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
 record Person(String name, int age){};
 @Configuration
 public class HelloWorldConfiguration {
@@ -17,7 +19,13 @@ public class HelloWorldConfiguration {
     }
 
     @Bean
+    @Primary
     public Person person(){
         return new Person("lala", 14);
+    }
+
+    @Bean(name = "person2methodCall")
+    public Person person2(){
+        return new Person(name(), age());
     }
 }
