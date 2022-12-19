4457596a222ea923211bd0d63f44cc7f7f9d4a99
nickync <lizheng9024@gmail.com>
2022-12-18 22:23:01 -0500

    content header

--- Udemy-Java/lesson-5-restful-web-services/src/main/java/com/example/restapi/restfulwebservices/controller/HelloWorldController.java
+++ Udemy-Java/lesson-5-restful-web-services/src/main/java/com/example/restapi/restfulwebservices/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.example.restapi.restfulwebservices.controller;
 
 import com.example.restapi.restfulwebservices.model.HelloWorld;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
     @GetMapping("hello-world")
     public String helloWorld(){
         return "Hello World";
@@ -21,5 +28,11 @@ public class HelloWorldController {
     @GetMapping("hello-world/path-variable/{username}")
     public HelloWorld helloWorldBean(@PathVariable String username){
         return new HelloWorld("Hello World");
+    }
+
+    @GetMapping("hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
