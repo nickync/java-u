471d546d2edb3ce4b74d1c6a18a692296075831f
nickync <lizheng9024@gmail.com>
2022-12-25 00:15:30 -0500

    router

--- Udemy-Java/lesson-6-fullstack/todo-app/src/App.js
+++ Udemy-Java/lesson-6-fullstack/todo-app/src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import Counter from './component/counter/Counter';
 import { useState } from 'react';
 import TodoApp, { LoginComponent, WelcomeComponent } from './component/todo/TodoApp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-      <TodoApp/>
-      <LoginComponent />
-      {/* <WelcomeComponent /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginComponent />}/>
+          <Route path='/login' element={<LoginComponent />}/>
+          <Route path='/welcome' element={<WelcomeComponent />} />
+        </Routes>
+        {/* <TodoApp/> */}
+      </BrowserRouter>
     </div>
   );
 }
