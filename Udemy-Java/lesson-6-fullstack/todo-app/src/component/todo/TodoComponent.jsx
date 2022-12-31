1934160ad88da31c99d7b6957739d2a74f3234dc
nickync <lizheng9024@gmail.com>
2022-12-31 00:21:17 -0500

    formik moment

--- Udemy-Java/lesson-6-fullstack/todo-app/src/component/todo/TodoComponent.jsx
+++ Udemy-Java/lesson-6-fullstack/todo-app/src/component/todo/TodoComponent.jsx
@@ -1,3 +1,4 @@
+import { ErrorMessage, Field, Form, Formik } from "formik"
 import { useState } from "react"
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
@@ -12,7 +13,9 @@ export default function TodoComponent() {
 
     const username = authContext.username
 
-    const [description, setDescription] = useState()
+    const [description, setDescription] = useState('')
+    const [targetDate, setTargetDate] = useState('')
+
 
     useEffect(
         () => {
@@ -24,14 +27,58 @@ export default function TodoComponent() {
         retrieveTodoApi(username, id).then(
             res => {
                 setDescription(res.data.description)
+                setTargetDate(res.data.targetDate)
             }
         )
+    }
+
+    const onSubmit = (values) => {
+
+    }
+
+    const validate = (values) => {
+        let errors = {
+            // description: 'Enter a valid description.',
+            // targetDate: 'Enter a valid date'
+        }
+
+        if (values.description.length < 5 ){
+            errors.description = 'Length must be longer than 5'
+        }
+        
+        if (values.targetDate == null){
+            errors.targetDate = 'Invalid date.'
+        }
+        return errors
     }
 
     return (
         <div className="container">
             <h1>Enter todo details</h1>
-            <h4>{description}</h4>
+            <div>
+                <Formik initialValues={{description, targetDate}} enableReinitialize={true} onSubmit={onSubmit} validate = {validate}
+                    validateOnChange={false} validateOnBlur={false}>
+                    {
+                        (props) => (
+                            <Form>
+                                <ErrorMessage name="description" component="div" className="alert alert-warning"/>
+                                <ErrorMessage name="targetDate" component="div" className="alert alert-warning"/>
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field className="form-control" name="description" />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Target date</label>
+                                    <Field type="date" className="form-control" name="targetDate" />
+                                </fieldset>
+                                <div>
+                                    <button type="submit" className="btn btn-success">Save</button>
+                                </div>
+                            </Form>
+                        )
+                    }
+                </Formik>
+            </div>
         </div>
     )
 }
