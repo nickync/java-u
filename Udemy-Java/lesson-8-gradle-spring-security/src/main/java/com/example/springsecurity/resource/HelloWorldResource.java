2412384aeedf2270f76ec7f1930ad0123b53e9c2
nickync <lizheng9024@gmail.com>
2023-01-14 23:56:33 -0500

    security

--- Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/resource/HelloWorldResource.java
+++ Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/resource/HelloWorldResource.java
@@ -1,16 +1,27 @@
 package com.example.springsecurity.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class HelloWorldResource {
 
-    @GetMapping("/hello-world")
-    public List<Todo> retrieveTodo(){
-        return List.of(new Todo("user", "AWS"),new Todo("user", "AWS"));
+    private static final List<Todo> TODOS =
+            List.of(new Todo("user", "AWS"),new Todo("user", "Quant"));
+
+    @GetMapping("/todos/{username}")
+    public List<Todo> retrieveTodo(@PathVariable String username){
+        return TODOS.stream().filter(i -> i.user().equalsIgnoreCase(username)).toList();
+    }
+
+    @PostMapping("/todos/{username}")
+    public void createTodo(@PathVariable String username, @RequestBody Todo todo){
+        TODOS.add(todo);
+    }
+    @GetMapping("/todos")
+    public List<Todo> retrieveAll(){
+        return TODOS;
     }
 }
 
