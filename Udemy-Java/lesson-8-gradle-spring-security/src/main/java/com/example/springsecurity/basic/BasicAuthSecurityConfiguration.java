28d1d4718d4387582785c69635e9dd356669c480
nickync <lizheng9024@gmail.com>
2023-01-16 23:38:15 -0500

    in memory user details

--- Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/basic/BasicAuthSecurityConfiguration.java
+++ Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +23,21 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic();
         http.csrf().disable();
         return http.build();
+    }
+
+    // in memory user details login/pw
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        var user = User.withUsername("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}user")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
