ebb0819aa4d05017caedfcee2090654098e0d1ec
nickync <lizheng9024@gmail.com>
2023-01-16 23:56:40 -0500

    frame option

--- Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/basic/BasicAuthSecurityConfiguration.java
+++ Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,12 +2,15 @@ package com.example.springsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -22,6 +25,7 @@ public class BasicAuthSecurityConfiguration {
         });
         http.httpBasic();
         http.csrf().disable();
+        http.headers().frameOptions().sameOrigin(); // allows frames from same origin
         return http.build();
     }
 
@@ -39,5 +43,10 @@ public class BasicAuthSecurityConfiguration {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
     }
 }
