054b7d9a167207649c78c27e13aaa276de6dc913
nickync <lizheng9024@gmail.com>
2023-01-16 23:26:14 -0500

    csrf

---
+++ Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/resource/SpringSecurityPlayResource.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity.resource;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSecurityPlayResource {
+
+    @GetMapping("/csrf-token")
+    public CsrfToken retrieveCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
