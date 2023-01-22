8e1046e5dd401fcc3f9c318c63b8fb21a056cb9e
nickync <lizheng9024@gmail.com>
2023-01-21 23:22:02 -0500

    jwt createToken

---
+++ Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/jwt/JwtAuthenticationResource.java
@@ -0,0 +1,45 @@
+package com.example.springsecurity.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+public class JwtAuthenticationResource {
+
+    @Autowired
+    private JwtEncoder jwtEncoder;
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(Authentication authentication) {
+        return new JwtResponse(createToken(authentication));
+    }
+
+    private String createToken(Authentication authentication){
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 30))
+                .subject(authentication.getName())
+                .claim("scope", createScope(authentication))
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims))
+                .getTokenValue();
+    }
+
+    private String createScope(Authentication authentication){
+        return authentication.getAuthorities().stream()
+                .map(a -> a.getAuthority())
+                .collect(Collectors.joining(" "));
+    }
+}
+
+record JwtResponse(String token){}
--- Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/jwt/JwtSecurityConfiguration.java
+++ Udemy-Java/lesson-8-gradle-spring-security/src/main/java/com/example/springsecurity/jwt/JwtSecurityConfiguration.java
@@ -1,8 +1,8 @@
 package com.example.springsecurity.jwt;
 
 import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.jwk.JWKSelector;
 import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +17,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,10 +27,7 @@ import javax.sql.DataSource;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPublicKey;
-import java.util.List;
 import java.util.UUID;
-
-import com.nimbusds.jose.jwk.RSAKey;
 
 @Configuration
 public class JwtSecurityConfiguration {
@@ -173,6 +172,11 @@ public class JwtSecurityConfiguration {
         return NimbusJwtDecoder
                 .withPublicKey(rsaKey.toRSAPublicKey())
                 .build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource){
+        return new NimbusJwtEncoder(jwkSource);
     }
 
 }
