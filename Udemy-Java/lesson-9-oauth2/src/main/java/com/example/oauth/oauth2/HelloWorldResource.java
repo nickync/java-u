4b679add4a5b548c6be0fc2c582930f547d0b901
nickync <lizheng9024@gmail.com>
2023-01-24 22:46:14 -0500

    oauth2 google sign in

---
+++ Udemy-Java/lesson-9-oauth2/src/main/java/com/example/oauth/oauth2/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package com.example.oauth.oauth2;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+
+    @GetMapping("/")
+    public String helloWorld(Authentication authentication){
+        System.out.println( authentication );
+        System.out.println(authentication.getPrincipal());
+        return "hello world";
+    }
+}
---
+++ Udemy-Java/lesson-9-oauth2/src/main/java/com/example/oauth/oauth2/OauthSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.oauth.oauth2;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class OauthSecurityConfiguration {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
+//        http.formLogin();
+//        http.httpBasic();
+        http.oauth2Login();
+        return http.build();
+    }
+
+}
