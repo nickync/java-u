44862b85f90fc1a3d63a86c70ef369f6cfb112ec
nickync <lizheng9024@gmail.com>
2023-02-06 00:13:35 -0500

    optional class

--- Udemy-Java/functional programming/PlayingWithOptional.java
+++ Udemy-Java/functional programming/PlayingWithOptional.java
@@ -1,10 +1,18 @@
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 public class PlayingWithOptional {
     public static void main(String[] args) {
         List<String> fruits = List.of("apple", "banana", "mango");
         Predicate<? super String> predicate = fruit -> fruit.startsWith("b");
-        fruits.stream().filter(predicate).forEach(System.out::println);
+        Optional<String> optional = fruits.stream().filter(predicate).findFirst();
+        System.out.println(optional);
+        System.out.println(optional.isEmpty());
+        System.out.println(optional.isPresent());
+        System.out.println(optional.get());
+
+        var fruit = Optional.of("peach");
+        System.out.println(fruit);
     }
 }
